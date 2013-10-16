c9e4de71d0ba4c115562af79eb28f8b2475def62
Li Ge <lge@ucsd.edu>
2013-10-16 08:59:45 -0700

    Update at Wed Oct 16 08:59:45 PDT 2013

--- test/examples.js
+++ test/examples.js
@@ -7,57 +7,91 @@ describe( 'Examples', function() {
       function() {
         var ISEModel = require( 'ise-fe-model' );
 
-        var m1 = ISEModel.create()
+        var history = [];
+        var m1 = ISEModel.create();
+        var m2 = ISEModel.create();
+
+        m1.on( 'delta', function( json ) {
+          m2.applyDelta( json );
+        } );
+
+        m1.on( 'commit', function( json ) {
+          m2.commit( json.uuid );
+          history.push( json );
+        } );
+
+        m1.commit();
+        m1
           .ndm( 2 )
-          .ndf( 2 );
+          .ndf( 2 )
+          .unit( 'kg,m,C' );
+        m1.commit();
 
-        var creator = m.factory();
+        m1.checkout( history[ 0 ] );
 
-        var mat = creator.FeMaterial.Uniaxial.Elatic.create()
-          .E( 3000 )
-          .addToFeModel( m1 );
+        expect( m1.toJSON() ).to.be.eql( m2.toJSON() );
 
-        var ts = creator.FeTimeSeries.Linear.create()
-          .addToFeModel( m1 );
+        var mat = m1.createMaterial
+          .type( 'Uniaxial.Elatic' )
+          .E( 3000 );
 
-        var n1 = creator.FeNode.create()
-          .positionX( 0.0 )
-          .positionY( 0.0 )
-          .fix()
-          .addToFeModel( m1 );
+        var ts = m1.createTimeSeries()
+          .type( 'Linear' );
 
-        var n2 = creator.FeNode.create()
-          .positionX( 144.0 )
-          .positionY( 0.0 )
-          .addToFeModel( m1 );
+        var n1 = m1.createNode()
+          .positionX( 0 )
+          .positionY( 0 );
 
-        var n3 = creator.FeNode.create()
-          .positionX( 168.0 )
-          .positionY( 0.0 )
-          .addToFeModel( m1 );
+        var n2 = m1.creatNode()
+          .positionX( 144 )
+          .positionY( 0 );
 
-        var n4 = creator.FeNode.create()
-          .positionX( 72.0 )
-          .positionY( 96 )
-          .addToFeModel( m1 );
+        var n3 = m1.createNode()
+          .positionX( 168 )
+          .positionY( 0 );
 
-        var e1 = creator.FeElement.Truss.create()
+        var n4 = m1.createNode()
+          .positionX( 72 )
+          .positionY( 96 );
+
+        var spcs = [ n1, n2, n3 ]
+          .map( function( n ) {
+            return m1.createConstraint()
+              .type( 'SinglePoint' )
+              .node( n );
+          } );
+
+        var l = m1.createLoad()
+          .type( 'NodalForce' )
+          .direction( 100, -50 )
+          .magnitude( 1 );
+
+        var e1 = m1.createElement()
+          .type( 'truss' )
           .nodes( n1, n4 )
           .A( 10 )
-          .material( mat )
-          .addToFeModel( m1 );
+          .material( mat );
 
-        var e2 = creator.FeElement.Truss.create()
-          .nodes( n2, n4 )
+        var e2 = m1.createElement()
+          .type( 'truss' )
+          .nodes( [ n2, n4 ] )
           .A( 5 )
-          .material( mat )
-          .addToFeModel( m1 );
+          .material( mat );
 
-        var e3 = creator.FeElement.Truss.create()
-          .nodes( n3, n4 )
+        var e3 = m1.createElement()
+          .type( 'truss' )
+          .nodes( [ n3, n4 ] )
           .A( 5 )
-          .material( mat )
-          .addToFeModel( m1 );
+          .material( mat );
+
+        var p = m1.createLoadPattern()
+          .type( 'Plain' )
+          .timeSeries( ts )
+          .loads( [ l ] );
+
+        console.log( m1.toJSON() );
+
+
 
       }
     );
